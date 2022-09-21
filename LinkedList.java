0318e2485d1b0e76fdf820c600d98ab6a8879a1e
SamaDawihi <samadawihi@gmail.com>
2022-09-21 16:45:39 +0300

    v2

--- LinkedList.java
+++ LinkedList.java
@@ -1,10 +1,37 @@
 public class LinkedList {
     private Node head;
     private Node current;
+    private int length;
 
     LinkedList(String path){
-        hjgj
+        
     }
+    void insert(String word, int line, int position, int length){
+
+    }
+    int getLength(){
+        return length;
+    }
+    int uniqueWordsNumber(){
+
+    }
+    int occurrenceNumber(String word){
+
+    }
+    int wordsWithLength(int wLength){
+
+    }
+    String sortByOccurrence(){
+
+    }
+    String location(String word){
+
+    }
+    boolean isAdjacent(String word1, String word2){
+        
+    }
+
+
 }
     
 
--- Node.java
+++ Node.java
@@ -3,12 +3,13 @@ class Node {
     Node next;
     int line;
     int position;
+    int length;
 
-    Node(String data, int line, int position) {
+    Node(String data, int line, int position, int length) {
         this.data = data;
         next = null;
         this.line = line;
         this.position = position;
     }
-    
+
 }
